77356979b9cab23a756ad930f338300ccd086838
unknown <tiagoaraujo84@gmail.com>
2021-10-30 12:29:36 -0300

    first commit

---
+++ tictactoe/lib/src/main/java/br/com/softblue/tictactoe/Main.java
@@ -0,0 +1,9 @@
+package br.com.softblue.tictactoe;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Jogo da velha");
+	}
+
+}
---
+++ tictactoe/lib/src/main/java/br/com/softblue/tictactoe/core/Game.java
@@ -0,0 +1,8 @@
+package br.com.softblue.tictactoe.core;
+
+public class Game {
+
+	public void play() {
+		System.out.println("Jogando...");
+	}
+}
